6f6316efc6456e752e89eb4200c5c1a326ebfa60
Aqib ahmad khan <94100627+Aqib-Ahmad@users.noreply.github.com>
2023-08-05 10:51:16 +0000

    dates

---
+++ js/6_dates.js
@@ -0,0 +1,8 @@
+// const  data = new Date(2023 , 0 , 5);
+// console.log(data.toLocaleString());
+
+// let createdDate = new Date("10-02-2023");
+// console.log(createdDate.toDateString());
+
+let dateNow = Date.now()
+console.log(dateNow);
