616cf7d59358a8cc52e3773429c9cb0139dd69ef
Aqib ahmad khan <94100627+Aqib-Ahmad@users.noreply.github.com>
2023-08-05 10:29:33 +0000

    min-max

---
+++ js/5_numbers.js
@@ -0,0 +1,24 @@
+// const data = 4000;
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(1));
+
+
+// console.log(Math);
+// console.log(Math.abs(-40));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.max(1,40,5,2,6));
+// console.log(Math.min(1,40,5,2,6));
+
+// imp-------- value shond be in b/w
+console.log(Math.floor(Math.random()*10));
+
+
+// sometimes we have min and max values 
+const min =10;
+const max = 20;
+
+console.log(Math.floor(Math.random() *(max-min + 1))+min);
