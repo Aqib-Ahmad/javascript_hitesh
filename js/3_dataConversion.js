eb5afbaeb34e048becaa72cb87cc16cb49ee86bb
Aqib ahmad khan <94100627+Aqib-Ahmad@users.noreply.github.com>
2023-08-05 07:00:21 +0000

    conversioN

---
+++ js/3_dataConversion.js
@@ -0,0 +1,25 @@
+// let score = "true";
+
+// console.log(typeof(score));
+
+// let variableNumber  = Number(score);
+// console.log(typeof(variableNumber));
+// console.log(variableNumber);
+
+
+
+// "33"=> 33
+// "33dfg" =>NaN
+// true => 1
+
+// let isLoggnedIn =1;
+// let isLoggnedIn ="";
+// let booleanIsLogginedIn = Boolean(isLoggnedIn)
+// console.log(booleanIsLogginedIn);
+
+// 1=>true
+// "" =>flse 
+// "khan"=>true
+
+
+
