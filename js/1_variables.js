fee4b1e2af2a093f2ba7b5be1362ddba31e6c197
Aqib ahmad khan <94100627+Aqib-Ahmad@users.noreply.github.com>
2023-08-05 06:46:48 +0000

    change

---
+++ js/1_variables.js
@@ -0,0 +1,13 @@
+const accountId = 102030;
+let accountEmail = "abc2gmail.com";
+var accountPassword = "0396";
+accontCity="bla";
+
+
+// NEVER USE VAR IN MODERN JS
+// accountId = 111;
+accountEmail ="abcdef@gmail.com"
+accountPassword = "040506"
+console.log(accountId);
+accontCity ="kashm"
+console.table([accountEmail , accountPassword , accontCity])
---
+++ js/2_dataTypes.js
@@ -0,0 +1,17 @@
+// "use strict" //treat all js as newer version  js 
+// alert("khan") // we are using node.js not browser 
+
+
+
+// DATATYPES :=>
+// Number 
+// String 
+// Boolean
+// null
+// undefined
+// Symbol
+
+
+
+// OBJECTS
+console.log(typeof(undefined));
